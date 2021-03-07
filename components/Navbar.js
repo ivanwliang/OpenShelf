aab87758a5cf6417ae6a9b2aa5b30841c60efc72
Ivan Liang <ivanwenliang@gmail.com>
2021-03-06 18:21:29 -0800

    Fix navbar routing to index instead of dashboard

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,29 +8,43 @@ export default function Navbar() {
   return (
     <header>
       <div className='max-w-7xl mx-auto px-4 sm:px-6'>
-        <div className='flex justify-between items-center border-b-2 border-gray-100 py-6 '>
-          <Link href='/'>
-            <a className='text-4xl font-extrabold'>OpenShelf</a>
-          </Link>
-          <div>
-            <Link href='/about'>
-              <a className=''>About</a>
-            </Link>
-            {auth.user ? (
-              <button
-                onClick={() => auth.signout()}
-                className='whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium bg-indigo-600 text-white'
-              >
-                Sign Out
-              </button>
-            ) : (
-              <Link href='/login'>
-                <a className='whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium bg-indigo-600 text-white'>
-                  Login
-                </a>
+        <div className='border-b-2 border-gray-100 py-6 '>
+          {auth.user ? (
+            <div className='flex justify-between items-center'>
+              <Link href='/dashboard'>
+                <a className='text-4xl font-extrabold'>OpenShelf</a>
               </Link>
-            )}
-          </div>
+              <div>
+                <Link href='/about'>
+                  <a className=''>About</a>
+                </Link>
+
+                <button
+                  onClick={() => auth.signout()}
+                  className='whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium bg-indigo-600 text-white'
+                >
+                  Sign Out
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className='flex justify-between items-center'>
+              <Link href='/'>
+                <a className='text-4xl font-extrabold'>OpenShelf</a>
+              </Link>
+              <div>
+                <Link href='/about'>
+                  <a className=''>About</a>
+                </Link>
+
+                <Link href='/login'>
+                  <a className='whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium bg-indigo-600 text-white'>
+                    Login
+                  </a>
+                </Link>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </header>
