fade2b123bb170900c94dfe3fb0fcd8fe13d6d8b
Ivan Liang <ivanwenliang@gmail.com>
2021-04-01 11:33:02 -0700

    Remove pagination

--- components/BookSearchbar.js
+++ components/BookSearchbar.js
@@ -1,35 +1,35 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const BookSearchbar = () => {
   const router = useRouter();
 
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     router.push({
-      pathname: '/search',
-      query: { q: search, page: 1 },
+      pathname: "/search",
+      query: { q: search },
     });
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit} className='flex rounded-md shadow-sm'>
+      <form onSubmit={handleSubmit} className="flex rounded-md shadow-sm">
         <input
-          id='bookSearch'
-          name='bookSearch'
-          type='text'
-          placeholder='Search for a book'
+          id="bookSearch"
+          name="bookSearch"
+          type="text"
+          placeholder="Search for a book"
           required
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          className='block w-full pl-6 border-gray-300 rounded-none rounded-l-md placeholder-gray-300 focus:ring-indigo-500 focus:border-indigo-500 sm:text-lg'
+          className="block w-full pl-6 border-gray-300 rounded-none rounded-l-md placeholder-gray-300 focus:ring-indigo-500 focus:border-indigo-500 sm:text-lg"
         />
         <button
-          type='submit'
-          className='-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'
+          type="submit"
+          className="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
         >
           <span>Search</span>
         </button>
--- pages/search.js
+++ pages/search.js
@@ -1,29 +1,23 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
-import Navbar from '@/components/Navbar';
-import BookCard from '@/components/BookCard';
-import BookSearchbar from '@/components/BookSearchbar';
+import Navbar from "@/components/Navbar";
+import BookCard from "@/components/BookCard";
+import BookSearchbar from "@/components/BookSearchbar";
 
 const searchResults = () => {
   const router = useRouter();
 
   const [books, setBooks] = useState([]);
-  const [searchPage, setSearchPage] = useState(1);
   const [numBooksFound, setNumBooksFound] = useState(0);
   const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const page = router.query.page;
-    setSearchPage(page);
-  }, [router.query.page]);
 
   useEffect(() => {
     const q = router.query.q;
 
     const fetchBooks = async (q) => {
       const response = await fetch(
-        `https://openlibrary.org/search.json?q=${q}&page=${searchPage}`
+        `https://openlibrary.org/search.json?q=${q}`
       );
 
       setLoading(true);
@@ -31,7 +25,7 @@ const searchResults = () => {
       const results = await response.json();
       // Remove results without covers and broken images
       const filteredResults = results.docs.filter(
-        (doc) => Object.keys(doc).includes('cover_i') && doc['cover_i'] !== -1
+        (doc) => Object.keys(doc).includes("cover_i") && doc["cover_i"] !== -1
       );
 
       setNumBooksFound(results.numFound);
@@ -43,22 +37,7 @@ const searchResults = () => {
     if (q) {
       fetchBooks(q);
     }
-  }, [router.query.q, searchPage]);
-
-  const incrementSearchPage = () => {
-    // 100 book results per page
-    const totalPages = numBooksFound / 100;
-    if (searchPage <= totalPages) {
-      setSearchPage(searchPage + 1);
-    }
-  };
-
-  const decrementSearchPage = () => {
-    if (searchPage == 1) {
-      return;
-    }
-    setSearchPage(searchPage - 1);
-  };
+  }, [router.query.q]);
 
   if (loading) {
     return null;
@@ -67,16 +46,16 @@ const searchResults = () => {
   return (
     <div>
       <Navbar />
-      <div className='max-w-7xl mx-auto px-4 sm:px-6 bg-white overflow-hidden sm:rounded-md mb-6'>
-        <div className='max-w-6xl mx-auto pt-6 pb-4 mt-2'>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 bg-white overflow-hidden sm:rounded-md mb-6">
+        <div className="max-w-6xl mx-auto pt-6 pb-4 mt-2">
           <BookSearchbar />
         </div>
         {/* {numBooksFound !== 0 && numBooksFound} */}
-        <p className='text-center text-gray-800 text-sm py-2'>
+        <p className="text-center text-gray-800 text-sm py-2">
           Didn't find your book? Try adding more keywords, such as the author's
           name.
         </p>
-        <ul className='divide-y divide-gray-200'>
+        <ul className="divide-y divide-gray-200">
           {books.map((book) => (
             <li key={book.key}>
               <BookCard
@@ -89,27 +68,6 @@ const searchResults = () => {
             </li>
           ))}
         </ul>
-
-        {/* Pagination */}
-        <nav
-          className='bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6'
-          aria-label='Pagination'
-        >
-          <div className='flex-1 flex justify-between sm:justify-end'>
-            <button
-              onClick={decrementSearchPage}
-              className='relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50'
-            >
-              Previous
-            </button>
-            <button
-              onClick={incrementSearchPage}
-              className='ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50'
-            >
-              Next
-            </button>
-          </div>
-        </nav>
       </div>
     </div>
   );
