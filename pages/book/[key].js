58aa910361b97d9b199ecb2e0d8239c4f334f214
Ivan Liang <ivanwenliang@gmail.com>
2021-04-01 21:20:56 -0700

    Prevent adding duplicate book

--- pages/book/[key].js
+++ pages/book/[key].js
@@ -1,26 +1,28 @@
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
-import Navbar from '@/components/Navbar';
-import BookSearchbar from '@/components/BookSearchbar';
-import { addBook } from '@/lib/db';
-import { useAuth } from '@/lib/auth';
+import Navbar from "@/components/Navbar";
+import BookSearchbar from "@/components/BookSearchbar";
+import { addBook, getUserBook } from "@/lib/db";
+import { useAuth } from "@/lib/auth";
 
 const BookDetail = () => {
   const auth = useAuth();
   const router = useRouter();
   const { key } = router.query;
 
-  const [authorKey, setAuthorKey] = useState('');
+  const [authorKey, setAuthorKey] = useState("");
   const [authorDetails, setAuthorDetails] = useState({});
+  const [userHasBook, setUserHasBook] = useState(false);
   const [bookDetails, setBookDetails] = useState({});
+
   const {
     title,
-    subtitle = '',
+    subtitle = "",
     authors,
     subjects = [],
     covers,
-    description = '',
+    description = "",
   } = bookDetails;
 
   useEffect(() => {
@@ -29,7 +31,6 @@ const BookDetail = () => {
       const result = await response.json();
 
       setBookDetails(result);
-      console.log(result);
       setAuthorKey(result.authors[0].author.key);
     };
 
@@ -51,12 +52,23 @@ const BookDetail = () => {
     }
   }, [authorKey]);
 
-  const handleClick = () => {
-    const authorName = authorDetails.name;
-    const authorBio = authorDetails?.bio?.value || '';
-    const cover = covers ? covers[0] : '';
+  useEffect(() => {
+    const fetchUserBook = async (key) => {
+      const response = await getUserBook(auth.user.uid, key);
+      if (response) {
+        setUserHasBook(true);
+      } else {
+        setUserHasBook(false);
+      }
+    };
+  });
 
-    if (auth.user) {
+  const handleClick = async () => {
+    const authorName = authorDetails.name;
+    const authorBio = authorDetails?.bio?.value || "";
+    const cover = covers ? covers[0] : "";
+
+    if (auth.user && !userHasBook) {
       addBook(auth.user.uid, key, {
         title,
         subtitle,
@@ -67,17 +79,17 @@ const BookDetail = () => {
         subjects,
       });
 
-      router.push('/dashboard');
+      router.push("/dashboard");
     } else {
-      router.push('/login');
+      router.push("/login");
     }
   };
 
   return (
     <div>
       <Navbar />
-      <main className='max-w-7xl mx-auto px-4 sm:px-6'>
-        <div className='max-w-5xl mx-auto py-12 mt-2'>
+      <main className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="max-w-5xl mx-auto py-12 mt-2">
           <BookSearchbar />
         </div>
 
@@ -97,13 +109,19 @@ const BookDetail = () => {
           {subjects &&
             subjects.map((subject) => <li key={subject}>{subject}</li>)}
         </ul>
-        <button
-          type='button'
-          className='inline-flex items-center mt-4 px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-          onClick={handleClick}
-        >
-          Add to Reading List
-        </button>
+        {userHasBook ? (
+          <button
+            type="button"
+            className="inline-flex items-center mt-4 px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={handleClick}
+          >
+            Add to Reading List
+          </button>
+        ) : (
+          <div className="inline-flex items-center mt-4 px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-green-400">
+            Book already added
+          </div>
+        )}
       </main>
     </div>
   );
